34dca15569ec560d4466c45634647fd35549fd83
KaptainCS3 <mbiapplgryn@gmail.com>
2021-11-21 22:42:26 +0100

    personal work

---
+++ Chapter 2/Programming Projects/manual_calendar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("\t\t\t\t\t\t\t\tMonth of January\n");
+    printf("\t\t\t\t\t\t\t\t\t\b\b2022\n");
+    printf("\t\t\t\t\t Sunday  Monday  Tuesday  Wednesday  Thursday  Friday  Saturday\n");
+    printf("\t\t\t\t\t                                                           1    \n");
+    printf("\t\t\t\t\t   2       3        4         5          6        7        8    \n");
+    printf("\t\t\t\t\t   9       10       11        12         13       14       15    \n");
+    printf("\t\t\t\t\t   16      17       18        19         20       21       22    \n");
+    printf("\t\t\t\t\t   23      24       25        26         27       28       29    \n");
+    printf("\t\t\t\t\t   30      31    \n");
+    return 0;
+}
